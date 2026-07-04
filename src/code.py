7b7e228d7717b9251e766ae665e0835eeecda200
Tiny-Turo <arturo.ros.cedaro@gmail.com>
2026-07-04 07:36:36 +0100

    moved lib so it all works

--- src/code.py
+++ src/code.py
@@ -1,7 +1,12 @@
+# fswatch -o src | xargs -n1 -I{} rsync -a --inplace --no-perms --no-owner --no-group \
+#   --exclude '__pycache__' \
+#   src/ /Volumes/CIRCUITPY/
+
+
 import time
-import src.screen as screen 
-import src.player as player
-import src.rotary_encoder as rotary_encoder
+import screen as screen 
+import player as player
+import rotary_encoder as rotary_encoder
 
 def press():
     player.shoot_ball(screen.group)
@@ -19,4 +24,3 @@ while True:
     player.update_balls(screen.group)
     rotary_encoder.update(press, turn)
     time.sleep(0.01)
-
